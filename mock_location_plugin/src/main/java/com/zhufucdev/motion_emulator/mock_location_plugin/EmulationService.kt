9c18ac60d3deae0da1fea18841be446075944b81
zhufucdev <1095291434@qq.com>
2023-05-07 07:30:38 +0800

    Bug: plugin freezes

--- mock_location_plugin/src/main/java/com/zhufucdev/motion_emulator/mock_location_plugin/EmulationService.kt
+++ mock_location_plugin/src/main/java/com/zhufucdev/motion_emulator/mock_location_plugin/EmulationService.kt
@@ -2,17 +2,22 @@ package com.zhufucdev.motion_emulator.mock_location_plugin
 
 import android.app.job.JobParameters
 import android.app.job.JobService
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 class EmulationService : JobService() {
+    private val scope = CoroutineScope(Dispatchers.Default)
+    private lateinit var waitJob: Job
+
     override fun onStartJob(params: JobParameters?): Boolean {
-        runBlocking {
+        waitJob = scope.launch {
             MockLocationProvider.wait()
         }
         return true
     }
 
     override fun onStopJob(params: JobParameters?): Boolean {
+        waitJob.cancel()
+        scope.cancel()
         return true
     }
 }
