6d51adb201141333eb5190741516005784f2d34b
zhufucdev <1095291434@qq.com>
2023-05-07 09:39:38 +0800

    Implemented unavailability notification

--- mock_location_plugin/src/main/java/com/zhufucdev/motion_emulator/mock_location_plugin/MockLocationProvider.kt
+++ mock_location_plugin/src/main/java/com/zhufucdev/motion_emulator/mock_location_plugin/MockLocationProvider.kt
@@ -1,7 +1,9 @@
 package com.zhufucdev.motion_emulator.mock_location_plugin
 
 import android.Manifest
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Build
@@ -11,6 +13,7 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.aventrix.jnanoid.jnanoid.NanoIdUtils
 import com.zhufucdev.data.*
+import com.zhufucdev.motion_emulator.mock_location_plugin.ui.MainActivity
 import com.zhufucdev.motion_emulator.mock_location_plugin.ui.TestFragment
 import io.ktor.client.*
 import io.ktor.client.call.*
@@ -214,11 +217,23 @@ object MockLocationProvider {
             return
         }
 
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            Intent(context, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            },
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
         val notification =
             NotificationCompat.Builder(context, CHANNEL_ID).setSmallIcon(R.drawable.ic_baseline_auto_fix_off)
                 .setContentTitle(context.getString(R.string.title_not_available))
                 .setContentText(context.getString(R.string.text_not_available))
-                .setPriority(NotificationCompat.PRIORITY_HIGH).build()
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build()
         with(NotificationManagerCompat.from(context)) {
             notify(0, notification)
         }
