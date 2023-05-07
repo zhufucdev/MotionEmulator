b7c7f732bae2a031189a4b536c3591ff351198cb
zhufucdev <1095291434@qq.com>
2023-05-07 10:31:28 +0800

    Implemented guidance

--- mock_location_plugin/src/main/java/com/zhufucdev/motion_emulator/mock_location_plugin/ui/GuidanceFragment.kt
+++ mock_location_plugin/src/main/java/com/zhufucdev/motion_emulator/mock_location_plugin/ui/GuidanceFragment.kt
@@ -1,13 +1,19 @@
 package com.zhufucdev.motion_emulator.mock_location_plugin.ui
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.snackbar.Snackbar
 import com.zhufucdev.motion_emulator.mock_location_plugin.R
+import com.zhufucdev.motion_emulator.mock_location_plugin.databinding.FragmentGuidanceBinding
 
 class GuidanceFragment : Fragment() {
+    private lateinit var binding: FragmentGuidanceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -15,8 +21,35 @@ class GuidanceFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guidance, container, false)
+        binding = FragmentGuidanceBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnOpenAbout.setOnClickListener {
+            tryStartActivity {
+                startActivity(Intent(android.provider.Settings.ACTION_DEVICE_INFO_SETTINGS))
+            }
+        }
+        binding.btnOpenDeveloper.setOnClickListener {
+            tryStartActivity {
+                startActivity(Intent(android.provider.Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS))
+            }
+        }
+    }
+
+    private fun tryStartActivity(block: () -> Unit) {
+        try {
+            block()
+        } catch (_: ActivityNotFoundException) {
+            Snackbar.make(
+                requireActivity().findViewById(R.id.fab),
+                R.string.text_activity_not_found,
+                Snackbar.LENGTH_INDEFINITE
+            ).show()
+        }
     }
 }
