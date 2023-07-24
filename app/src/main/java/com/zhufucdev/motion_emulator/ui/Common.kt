461ce9e28ef4ac07c74cb845be33db9637a2c894
zhufucdev <1095291434@qq.com>
2023-07-24 18:07:44 +0800

    Removed unused imports

--- app/src/main/java/com/zhufucdev/motion_emulator/ui/Common.kt
+++ app/src/main/java/com/zhufucdev/motion_emulator/ui/Common.kt
@@ -1,39 +1,15 @@
 package com.zhufucdev.motion_emulator.ui
 
-import android.util.Log
-import androidx.compose.animation.core.Animatable
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment.Companion.BottomStart
-import androidx.compose.ui.Alignment.Companion.CenterEnd
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Alignment.Companion.CenterStart
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.LayoutDirection
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import com.zhufucdev.motion_emulator.ui.theme.paddingCommon
-import com.zhufucdev.motion_emulator.ui.theme.paddingSmall
-import kotlin.math.abs
-import kotlin.math.roundToInt
-import kotlin.math.sign
 
 @Composable
 fun VerticalSpacer(space: Dp = paddingCommon) {
