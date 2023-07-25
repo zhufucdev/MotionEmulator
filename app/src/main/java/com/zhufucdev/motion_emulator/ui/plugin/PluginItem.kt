c9505b96403bfc56b2dec82102237046a3e3f3cb
zhufucdev <1095291434@qq.com>
2023-07-25 22:51:29 +0800

    Compose optimization

--- app/src/main/java/com/zhufucdev/motion_emulator/ui/plugin/PluginItem.kt
+++ app/src/main/java/com/zhufucdev/motion_emulator/ui/plugin/PluginItem.kt
@@ -1,16 +1,42 @@
 package com.zhufucdev.motion_emulator.ui.plugin
 
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.zhufucdev.api.ProductQuery
 import com.zhufucdev.motion_emulator.plugin.Plugin
 import com.zhufucdev.motion_emulator.plugin.Plugins
 
-data class PluginItem(
+@Stable
+class PluginItem(
     val id: String,
     val title: String,
     val subtitle: String = "",
-    var enabled: Boolean,
-    var state: PluginItemState
-)
+    enabled: Boolean,
+    state: PluginItemState
+) {
+    var enabled by mutableStateOf(enabled)
+    var state by mutableStateOf(state)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PluginItem
+
+        if (id != other.id) return false
+        if (enabled != other.enabled) return false
+        return state == other.state
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + enabled.hashCode()
+        result = 31 * result + state.hashCode()
+        return result
+    }
+}
 
 enum class PluginItemState {
     NOT_DOWNLOADED, NONE, UPDATE
