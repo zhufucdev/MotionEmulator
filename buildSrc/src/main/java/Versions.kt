3fd1a8e6bfd81a478dd0b37a341a5880decde0ba
zhufucdev <1095291434@qq.com>
2023-08-07 15:11:32 +0800

    Upgrade to sdk 34

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,14 +1,14 @@
 object Versions {
     const val minSdk = 24
-    const val targetSdk = 33
+    const val targetSdk = 34
 
     const val appcompatVersion = "1.6.1"
-    const val coreKtVersion = "1.10.1"
+    const val coreKtVersion = "1.12.0-beta01"
     const val coroutineVersion = "1.7.1"
     const val composeUiVersion = "1.4.3"
     const val composeCompilerVersion = "1.5.0"
     const val composeActivityVersion = "1.7.2"
-    const val composeMaterialVersion = "1.2.0-alpha02"
+    const val composeMaterialVersion = "1.2.0-alpha04"
     const val ktorVersion = "2.3.2"
     const val kotlinVersion = "1.9.0"
     const val kotlinxSerializationVersion = "1.5.1"
