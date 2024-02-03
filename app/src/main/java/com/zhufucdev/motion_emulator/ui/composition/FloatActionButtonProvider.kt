db4c147fd9a4b43e4584e3bcd70677725e0e4225
zhufucdev <1095291434@qq.com>
2024-02-03 18:29:53 +0800

    :bug: Visual bug

--- app/src/main/java/com/zhufucdev/motion_emulator/ui/composition/FloatActionButtonProvider.kt
+++ app/src/main/java/com/zhufucdev/motion_emulator/ui/composition/FloatActionButtonProvider.kt
@@ -2,14 +2,19 @@ package com.zhufucdev.motion_emulator.ui.composition
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.SizeTransform
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.unit.dp
 
 data class FloatActionButtonProvider(val manipulator: FloatingActionButtonManipulator)
 
@@ -40,6 +45,7 @@ object DefaultFloatingActionButtonManipulator : FloatingActionButtonManipulator 
                 ContentTransform(
                     targetContentEnter = scaleIn(),
                     initialContentExit = scaleOut(),
+                    sizeTransform = SizeTransform(clip = false)
                 )
             }
         ) {
